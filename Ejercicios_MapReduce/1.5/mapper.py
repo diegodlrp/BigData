eb227d54f1b1d02e6c16537f1e139bcfe67fae53
diegodlrp <diegodlrp2000@gmail.com>
2022-01-13 16:51:30 +0100

    2º commit

--- Ejercicios_MapReduce/1.5/mapper.py
+++ Ejercicios_MapReduce/1.5/mapper.py
@@ -11,4 +11,4 @@ import sys
 for line in sys.stdin:
     data = line.strip().split("\t")
     date, time, store, item, cost, payment = data
-    print(f'{payment}\t{cost}')
+    print(f'{item}\t{cost}')
